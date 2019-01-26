e39c57080273fc24387977688d183a9c65f84722
JamieCovell <jamiec.covellj@gmail.com>
2019-01-26 01:13:21 +0000

    StartedQuests

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    public QuestSO activeQuest;
+
+    public void StartQuest (QuestSO q)
+    {
+
+        GameObject obj = q.target;
+        Transform targSpawn = q.spawnPoint;
+        Transform targEnd = q.targetPoint;
+        string questText = q.questText;
+
+        Debug.Log (string.Format (questText, obj.name));
+
+    }
+
+}
