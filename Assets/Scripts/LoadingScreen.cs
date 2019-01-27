bfdd130271778fe10409babb7ffb70cf1ea4d18b
Jamie Covell <jamiec.covellj@gmail.com>
2019-01-27 15:02:28 +0000

    Loading

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour {
+
+    public Image[] shafts;
+    private AsyncOperation loading;
+
+    private void Start()
+    {
+
+        loading = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void Update()
+    {
+        foreach(var shaft in shafts)
+        {
+            shaft.fillAmount = loading.progress;
+        }
+    }
+
+}
